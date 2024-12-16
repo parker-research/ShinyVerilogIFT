8b4a45c3437849eded34c2e892295445ea44094a
parker-research <166864283+parker-research@users.noreply.github.com>
2024-12-16 15:37:16 -0700

    Add test input/outputs for all gate types

--- tests/test_boolean_op_transforms.py
+++ tests/test_boolean_op_transforms.py
@@ -3,9 +3,8 @@
 import pytest
 
 
-def test_single_bit_and_operation():
+def test_single_bit_and_operation() -> None:
     """Test the `and_transform` function."""
-
     input_module = """
         module and_gate (
             input wire x,
@@ -16,6 +15,9 @@ def test_single_bit_and_operation():
         endmodule
     """
 
+    # Conservative Source: (Ardeshiricham et al. 2017), Figure 3, Part a
+    # Precise Source: (Ardeshiricham et al. 2017), Figure 3, Part b
+    # Precise Source: (Hu et al. 2011), Equation 1
     expected_output = """
         module and_gate (
             input wire x,
@@ -30,7 +32,7 @@ def test_single_bit_and_operation():
         );
             assign z = x & y;
             assign z_tag_conservative_prop0_ABCD = (
-                x_tag_conservative_prop0_ABCD & y_tag_conservative_prop0_ABCD
+                x_tag_conservative_prop0_ABCD | y_tag_conservative_prop0_ABCD
             );
             assign z_tag_precise_prop0_ABCD = (
                 (x_tag_precise_prop0_ABCD & y_tag_precise_prop0_ABCD) |
@@ -40,4 +42,120 @@ def test_single_bit_and_operation():
         endmodule
     """
 
-    assert input_module == expected_output
+    raise NotImplementedError
+
+
+def test_single_bit_or_operation() -> None:
+    """Test the `or_transform` function."""
+    input_module = """
+        module or_gate (
+            input wire g,
+            input wire h,
+            output wire f
+        );
+            assign f = g | h;
+        endmodule
+    """
+
+    # Precise Source: (Hu et al. 2011), Equation 7
+    expected_output = """
+        module or_gate (
+            input wire g,
+            input wire g_tag_conservative_prop0_ABCD,
+            input wire g_tag_precise_prop0_ABCD,
+            input wire h,
+            input wire h_tag_conservative_prop0_ABCD,
+            input wire h_tag_precise_prop0_ABCD,
+            output wire f,
+            output wire f_tag_conservative_prop0_ABCD,
+            output wire f_tag_precise_prop0_ABCD
+        );
+            assign f = g & h;
+            assign f_tag_conservative_prop0_ABCD = (
+                g_tag_conservative_prop0_ABCD | h_tag_conservative_prop0_ABCD
+            );
+            assign f_tag_precise_prop0_ABCD = (
+                (g_tag_precise_prop0_ABCD & h_tag_precise_prop0_ABCD) |
+                ((~g) & h_tag_precise_prop0_ABCD) |
+                ((~h) & g_tag_precise_prop0_ABCD)
+            );
+        endmodule
+    """
+
+    raise NotImplementedError
+
+
+def test_single_bit_xor_operation() -> None:
+    """Test the `xor_transform` function."""
+    input_module = """
+        module xor_gate (
+            input wire g,
+            input wire h,
+            output wire f
+        );
+            assign f = g ^ h;
+        endmodule
+    """
+
+    # Precise Source: (Hu et al. 2011), Equation 10
+
+    # Interesting property: Conservative and precise tags are the same calculation.
+    expected_output = """
+        module xor_gate (
+            input wire g,
+            input wire g_tag_conservative_prop0_ABCD,
+            input wire g_tag_precise_prop0_ABCD,
+            input wire h,
+            input wire h_tag_conservative_prop0_ABCD,
+            input wire h_tag_precise_prop0_ABCD,
+            output wire f,
+            output wire f_tag_conservative_prop0_ABCD,
+            output wire f_tag_precise_prop0_ABCD
+        );
+            assign f = g & h;
+            assign f_tag_conservative_prop0_ABCD = (
+                g_tag_conservative_prop0_ABCD | h_tag_conservative_prop0_ABCD
+            );
+            assign f_tag_precise_prop0_ABCD = (
+                g_tag_conservative_prop0_ABCD | h_tag_conservative_prop0_ABCD
+            );
+        endmodule
+    """
+
+    raise NotImplementedError
+
+
+def test_single_bit_not_operation() -> None:
+    """Test the `not_transform` function."""
+    input_module = """
+        module not_gate (
+            input wire g,
+            output wire f
+        );
+            assign f = ~g;
+        endmodule
+    """
+
+    # Precise Source: (Hu et al. 2011), Equation 10
+
+    # Interesting property: Conservative and precise tags are the same calculation.
+    expected_output = """
+        module not_gate (
+            input wire g,
+            input wire g_tag_conservative_prop0_ABCD,
+            input wire g_tag_precise_prop0_ABCD,
+            output wire f,
+            output wire f_tag_conservative_prop0_ABCD,
+            output wire f_tag_precise_prop0_ABCD
+        );
+            assign f = g & h;
+            assign f_tag_conservative_prop0_ABCD = (
+                g_tag_conservative_prop0_ABCD
+            );
+            assign f_tag_precise_prop0_ABCD = (
+                g_tag_conservative_prop0_ABCD
+            );
+        endmodule
+    """
+
+    raise NotImplementedError
